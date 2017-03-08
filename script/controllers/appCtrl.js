f6a6d5fb47079323c94f1fab68adea9e7e3fb4f2
Shrawan <incepligator@gmail.com>
2017-03-08 12:35:40 -0600

    angualr repeat and angular array

--- script/controllers/appCtrl.js
+++ script/controllers/appCtrl.js
@@ -24,14 +24,68 @@ angular.module("app").controller("appCtrl",
                                          "lastName":"Shrestha",
                                          "phone":"421385",
                                      }
+                          
                                     
+                        $scope.sampleArray=["cat","dog","bat"];           
+                          
+                         $scope.addAnimal=function(){
+                             $scope.sampleArray.push($scope.animal);
+                             
+                         }            
+                                     
                                     
+                                          var person1={
+                               "firstName":"Newyork",
+                                         "lastName":"Lama",
+                                         "phone":"00000000",
+                             
+                         } 
+                                          
+                                var person2={
+                               "firstName":"Rock",
+                                         "lastName":"Pradhan",
+                                         "phone":"999999",
+                             
+                         } 
+                                        var person3={
+                               "firstName":"Monica",
+                                         "lastName":"Clintion ",
+                                         "phone":"7877878787",
+                             
+                         }  
+                                
+                                    
+                         $scope.personArray=[];
+                                     
+                                     $scope.personArray.push(person1);
+                                      $scope.personArray.push(person2);
+                                      $scope.personArray.push(person3);
+                                    
+                                    console.log($scope.personArray[0]);
+                                     
+                                    
+                                     
+                                     
+                                     
                                     $scope.sampleSubmit=function(){
                                         
                                         event.preventDefault();
-
-                                        console.log($scope.formModel);
+                                        var samplePerson={
+                                            
+                                            "firstName":$scope.formModel.firstName,
+                                             "lastName":$scope.formModel.lastName,
+                                             "phone":$scope.formModel.phone,
+                                        }
                                         
+                                        //for empty default value 
+                                        $scope.formModel={};
+                                        
+                                        $scope.personArray.push(samplePerson);
+                                        console.log($scope.formModel);
+                                        console.log($scope.personArray);
                                     }
                                      
                                   }]);
+
+
+                    
